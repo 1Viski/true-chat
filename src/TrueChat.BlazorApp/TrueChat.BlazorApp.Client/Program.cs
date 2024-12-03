101c8bd2db2bbde0a7a913f199e5565f7220ddf3
Viski <umi0n3@gmail.com>
2024-12-04 00:56:57 +0100

    Add MudBlazor and minor changes and fixing

--- src/TrueChat.BlazorApp/TrueChat.BlazorApp.Client/Program.cs
+++ src/TrueChat.BlazorApp/TrueChat.BlazorApp.Client/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 using TrueChat.BlazorApp.Client.Clients;
 using TrueChat.BlazorApp.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddMudServices();
 
 builder.Services.AddHttpClient<IChatClient, ChatClient>();
 builder.Services.AddScoped<IChatService, ChatService>();
--- src/TrueChat.BlazorApp/TrueChat.BlazorApp/Program.cs
+++ src/TrueChat.BlazorApp/TrueChat.BlazorApp/Program.cs
@@ -1,6 +1,4 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using MudBlazor.Services;
 using TrueChat.BlazorApp.Client.Clients;
 using TrueChat.BlazorApp.Client.Services;
 using TrueChat.BlazorApp.Components;
@@ -11,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddHttpClient<IChatClient, ChatClient>();
 builder.Services.AddScoped<IChatService, ChatService>();
