c44d1f7a4287334703d0825c073c7535e106db7f
Minato <umi0n3@gmail.com>
2024-12-06 13:55:21 +0100

    View

--- src/TrueChat.BlazorApp/TrueChat.BlazorApp.Client/Models/ChatMessage.cs
+++ src/TrueChat.BlazorApp/TrueChat.BlazorApp.Client/Models/ChatMessage.cs
@@ -2,4 +2,8 @@ using TrueChat.BlazorApp.Client.Enums;
 
 namespace TrueChat.BlazorApp.Client.Models;
 
-public record ChatMessage(string? Text, string? Nickname, DateTimeOffset SendAt, DocumentSentiment? Sentiment);
+public record ChatMessage(
+    string? Text, 
+    string? Nickname, 
+    DateTimeOffset SendAt, 
+    DocumentSentiment? Sentiment);
